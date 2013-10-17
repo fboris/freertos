6139842f25fe0ea4f1c8dc8e729671a451fc9d1a
Shengwen1997 <l1996812@gmail.com>
2013-10-17 23:44:57 +0800

    Added new C lib function in string.c
    (1)Replaced the old strlen() to the ARM optimized implementation
    (2)Added memmove() function

--- string.c
+++ string.c
@@ -1,10 +1,41 @@
+#include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <limits.h>
 
-size_t strlen(const char *str)
+#define ALIGN (sizeof(size_t))
+#define ONES ((size_t)-1/UCHAR_MAX)
+#define HIGHS (ONES * (UCHAR_MAX/2+1))
+#define HASZERO(x) ((x)-ONES & ~(x) & HIGHS)
+
+size_t strlen(const char *s)
 {
-        const char *s;
+        const char *a = s;
+        const size_t *w;
+        for (; (uintptr_t) s % ALIGN; s++)
+                if (!*s) return (s - a);
+        for (w = (const void *) s; !HASZERO(*w); w++);
+        for (s = (const void *) w; *s; s++);
+        return (s - a);
+}
 
-        for (s = str; *s; ++s)
-                ;
-        return (s - str);
-}                          
+void *memmove(void *dest, const void *src, size_t count)
+{
+	char *tmp;
+	const char *s;
+
+	if(dest <= src) {
+		tmp = dest;
+		s = src;
+		while(count--)
+			*tmp++ = *s++;
+	} else {
+		tmp = dest;
+		tmp += count;
+		s= src;
+		s += count;
+		while(count--)
+			*--tmp = *--s;
+	}
+	return dest;
+}                   
