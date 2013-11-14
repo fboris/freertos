a71e3af8303c09cbc5f8c700efcd6cab2abaf1c9
fboris <fantasyboris@gmail.com>
2013-11-14 22:30:52 +0800

    Remove USART unused code

--- lib/Utilities/STM32F4-Discovery/stm32f4_discovery.h
+++ lib/Utilities/STM32F4-Discovery/stm32f4_discovery.h
@@ -149,29 +149,6 @@ typedef enum
   COM2 = 1
 } COM_TypeDef;
 
-#define COMn                             1
-
-/**
- * @brief Definition for COM port1, connected to USART3
- */
-#define EVAL_COM1                        USART3
-#define EVAL_COM1_CLK                    RCC_APB1Periph_USART3
-#define EVAL_COM1_TX_PIN                 GPIO_Pin_10
-#define EVAL_COM1_TX_GPIO_PORT           GPIOC
-#define EVAL_COM1_TX_GPIO_CLK            RCC_AHB1Periph_GPIOC
-#define EVAL_COM1_TX_SOURCE              GPIO_PinSource10
-#define EVAL_COM1_TX_AF                  GPIO_AF_USART3
-#define EVAL_COM1_RX_PIN                 GPIO_Pin_11
-#define EVAL_COM1_RX_GPIO_PORT           GPIOC
-#define EVAL_COM1_RX_GPIO_CLK            RCC_AHB1Periph_GPIOC
-#define EVAL_COM1_RX_SOURCE              GPIO_PinSource11
-#define EVAL_COM1_RX_AF                  GPIO_AF_USART3
-#define EVAL_COM1_IRQn                   USART3_IRQn
-
-//Protos
-void STM_EVAL_COMInit(COM_TypeDef COM, USART_InitTypeDef* USART_InitStruct);
-
-//void STM_EVAL_COMInit(COM_TypeDef COM);
 void init_rs232();
 void enable_rs232_interrupts(void);
 
